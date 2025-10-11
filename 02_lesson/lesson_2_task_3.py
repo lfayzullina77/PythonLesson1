753bcf34a158b49f97f719ad1a177eb2cb3e9ae8
lfayzullina77 <lfayzullina77@yandex.ru>
2025-10-11 07:50:35 +0300

    correction task3

--- 02_lesson/lesson_2_task_3.py
+++ 02_lesson/lesson_2_task_3.py
@@ -5,5 +5,5 @@ def square(items):
     return math.ceil(items * items)
 
 
-num_items = float(input("ВВведите длину строны квадрата: "))
+num_items = float(input("ВВведите длину стороны квадрата: "))
 print(f"Округленная в большую сторону площадь квадрата: {square(num_items)}")
